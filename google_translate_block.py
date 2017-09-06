a7d8fe32434a8306ec7edd59e08dbc280e0b69cc
neutralioJenkins <jenkins@n.io>
2017-09-06 08:35:04 -0600

    Bump version to v0.1.0

--- google_translate_block.py
+++ google_translate_block.py
@@ -7,7 +7,7 @@ from googletrans import Translator
 
 class GoogleTranslate(Block):
 
-    version = VersionProperty('0.1.0')
+    version = VersionProperty("0.1.0")
     src = StringProperty(title='Source Language')
     dest = StringProperty(title='Destination Language')
 
